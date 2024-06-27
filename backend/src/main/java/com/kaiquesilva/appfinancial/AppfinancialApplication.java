e947010899453e539770f2f58d350f72b6d63a05
Kaique <kaique.andruid@gmail.com>
2024-06-27 12:16:20 -0300

    first commit

---
+++ backend/src/main/java/com/kaiquesilva/appfinancial/AppfinancialApplication.java
@@ -0,0 +1,13 @@
+package com.kaiquesilva.appfinancial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppfinancialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppfinancialApplication.class, args);
+	}
+
+}
