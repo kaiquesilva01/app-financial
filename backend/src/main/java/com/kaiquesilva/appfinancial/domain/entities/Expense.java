2a40aad77dbe128e063107b08b39dc0688e291e7
Notebook <kaique.andruid@gmail.com>
2024-07-02 13:51:56 -0300

    expense fix

--- backend/src/main/java/com/kaiquesilva/appfinancial/domain/entities/Expense.java
+++ backend/src/main/java/com/kaiquesilva/appfinancial/domain/entities/Expense.java
@@ -2,10 +2,15 @@ package com.kaiquesilva.appfinancial.domain.entities;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "tb_expenses")
-public class expense {
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class Expense {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
